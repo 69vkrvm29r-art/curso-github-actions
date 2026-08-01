7441daece6bf46d6bbd72e648b75611f640b5dd3
69vkrvm29r-art <69vkrvm29r@privaterelay.appleid.com>
2026-08-01 17:22:39 -0600

    Update S2-h_lenguaje.py

--- S2-h_lenguaje.py
+++ S2-h_lenguaje.py
@@ -1,1 +1,10 @@
+import os
 
+def main():
+    nombre = os.getenv("USERNAME")
+    programa = os.getenv("LANGUAGE")
+    print(f"Hola, {nombre} desde GitHub usando {programa} >_<")
+
+
+if __name__ == "__main__":
+   main()
